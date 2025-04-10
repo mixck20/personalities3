5b036a675ec9e9f412bf91616ab7e520d300b623
mixck <deloriamicko@gmail.com>
2025-04-11 01:14:37 +0800

    PersonalityRepository.java

--- backend/personality/src/main/java/com/deloria/personality/PersonalityRepository.java
+++ backend/personality/src/main/java/com/deloria/personality/PersonalityRepository.java
@@ -3,5 +3,5 @@ package com.deloria.personality;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PersonalityRepository extends CrudRepository<Personality, Integer> {
-    // Add custom queries here if needed
+
 }
